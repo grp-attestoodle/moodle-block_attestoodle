b3b8022be625d0dc6d95603f7fcbbe3c3589e408
Leconte <marc.leconte@univ-lemans.fr>
2019-11-04 09:30:57 +0100

    Add files via upload

---
+++ lang/en/block_attestoodle.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_attestoodle
+ * @category    string
+ * @copyright   2019 Marc Leconte <Marc.Leconte@univ-lemans.fr>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['certificate'] = 'Certifications';
+$string['configtitle'] = 'Display for learners';
+$string['coursemilestones'] = 'Course milestones ';
+$string['coursestraining'] = ' Training courses ';
+$string['dateformat'] = 'Y-m-d';
+$string['displaycertificatelist'] = 'Access to the list of certificates';
+$string['displaynextcertificate'] = 'View the next certificate generation deadline';
+$string['displayprogress'] = 'Display the progress estimate';
+$string['displayratiomilestones'] = 'Display the number of milestones crossed on the course';
+$string['inadvance'] = 'In advance of ';
+$string['intime'] = 'In time';
+$string['lateby'] = 'Late by ';
+$string['lstcertificate'] = 'List of certificates';
+$string['milestones'] = 'Milestones';
+$string['nextdeadline'] = 'Next deadline ';
+$string['numbercertificate'] = 'Number of certificates : ';
+$string['numbertraining'] = 'Number of training : ';
+$string['numbermilestones'] = 'Number of milestone : ';
+$string['pluginname'] = 'Attestoodle';
+$string['privacy:metadata'] = 'The Attestoodle block does not record any personal data.';
+$string['timeend'] = 'End';
+$string['timegenerated'] = 'Date generated';
+$string['timestart'] = 'Begin';
+$string['traininglist'] = 'Training List';
+$string['warning'] = 'ATTENTION THIS COURSE IS ABOUT MILESTONES';
+$string['yourprogress'] = 'your progress ';
